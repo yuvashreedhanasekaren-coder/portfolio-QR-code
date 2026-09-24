7cc4af19eb27dbfcf1a6885f0353bdcb149d87f4
Yuvashree <yuvashreedhanasekaren@gmail.com>
2026-09-24 21:26:29 +0530

    Improve QR code generation reliability

--- qr_generator.py
+++ qr_generator.py
@@ -1,9 +1,18 @@
+import os
 import qrcode
+
 from config import PORTFOLIO_URL
 
 def generate_qr_code():
+    output_directory = "static"
+    output_file = os.path.join(output_directory, "portfolio_qr.png")
+
+    os.makedirs(output_directory, exist_ok=True)
+
     qr = qrcode.make(PORTFOLIO_URL)
-    qr.save("static/portfolio_qr.png")
+    qr.save(output_file)
+
+    return output_file
 
 if __name__ == "__main__":
     generate_qr_code()
